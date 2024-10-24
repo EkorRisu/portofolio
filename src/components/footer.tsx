0ba32600dc03b06007e86c2de1c17dcf782594a2
EkorRisu <wayanfebrian05@gmail.com>
2024-10-24 07:45:44 +0700

    improve

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, } from "react-icons/fa";
 import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
