20d0c5e6c27c414ff895ef3dd4ab2a601e868820
EkorRisu <wayanfebrian05@gmail.com>
2024-10-24 07:45:12 +0700

    improve

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -12,17 +13,17 @@ const Footer = () => {
 
         {/* Ikon Media Sosial */}
         <div className="flex space-x-6 text-lg">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+          <a href="https://web.facebook.com/wayan.febrian.528/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
             <FaFacebookF />
           </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://www.instagram.com/tegar_wayan08/" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <FaInstagram />
           </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
-            <FaTwitter />
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
-            <FaLinkedinIn />
+          <a href="https://github.com/EkorRisu" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+            <FaGithub />
+        </a>
+          <a href="https://x.com/Bukan_Wibu05" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
+            <FaXTwitter />
           </a>
         </div>
 
